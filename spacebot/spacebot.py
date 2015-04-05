5d09f68db6213f005416873e57947a3ef2c40ade
Chris Campo <ccampo133@users.noreply.github.com>
2015-04-04 22:19:13 -0400

    Added logging.

--- spacebot/spacebot.py
+++ spacebot/spacebot.py
@@ -1,20 +1,22 @@
 """Usage:
-  spacebot (-c CHANNEL) (-t TOKEN) (-T TIME)
+  spacebot (-c CHANNEL) (-t TOKEN) [-T TIME] [-f LOGFILE] [-l LEVEL]
   spacebot (-h | --help)
 
   Options:
-  -c CHANNEL --channel=CHANNEL    The Slack channel/group ID.
-  -t TOKEN --token=TOKEN          Your Slack API token
-  -T --time=TIME                  The time of day to run (in 24 hour format HH:mm, e.g. 11:00)
-  -h --help                       Show this screen.
+  -c CHANNEL --chan=CHANNEL   The Slack channel or group ID.
+  -t TOKEN --token=TOKEN      Your Slack API token.
+  -T --time=TIME              The time of day to run (in 24 hour format HH:mm, e.g. 11:00). [default: 12:00]
+  -f --file=LOGFILE           The file to output all logging info. [default: spacebot.log]
+  -l --level=LEVEL            The logging level: DEBUG, INFO, WARNING, ERROR, or CRITICAL. [default: INFO]
+  -h --help                   Show this screen.
 """
 import json
 import urllib2
 import datetime
 import time
+import logging
 
 import re
-
 import schedule
 from docopt import docopt
 from slacker import Slacker
@@ -38,11 +40,12 @@ def get_apod_data():
     text = re.findall('Explanation:(.*?)Tomorrow', soup.get_text(), re.DOTALL | re.MULTILINE)[0]
     text = text.replace('\n', ' ').rstrip().lstrip()
     text = re.sub("\s\s+", " ", text)  # removes double spaces
+    logging.debug('title = %s, img_url = %s, text = %s' % (title, img_url, text))
     return title, img_url, text
 
 
 def send_message(slack_client, chan):
-    title, text, img_url = get_apod_data()
+    title, img_url, text = get_apod_data()
     attachments = json.dumps([
         {
             'title': title,
@@ -54,21 +57,31 @@ def send_message(slack_client, chan):
             'author_icon': author_icon
         }
     ])
-    print(title, text, img_url, attachments)
     msg_title = 'Astronomy Picture of the Day - ' + datetime.datetime.now().strftime("%a %B %d, %Y")
     slack_client.chat.post_message(chan, msg_title, username='SpaceBot', icon_url=spacebot_icon_url,
                                    attachments=attachments)
+    logging.info('Sent message: chan = %s, msg_title = %s, attachments = %s' % (chan, msg_title, attachments))
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
     token = args['--token']
-    channel = args['--channel']
+    channel = args['--chan']
     run_time = args['--time']
-    slack = Slacker(token)  # Slack client (using the 'Slacker' library)
+    log_file = args['--file']
+    log_level = args['--level']
 
+    # Set up logging stuff
+    # TODO - add timestamps and more advanced logging configuration
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % log_level)
+    logging.basicConfig(filename=log_file, level=numeric_level)
+
+    slack = Slacker(token)  # Slack client (using the 'Slacker' library)
     schedule.every().day.at(run_time).do(send_message, slack, channel)
 
+    logging.info("SpaceBot is scheduled to run every day at %s" % run_time)
     while True:
         schedule.run_pending()
         time.sleep(1)
